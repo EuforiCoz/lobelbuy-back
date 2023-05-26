6068e698467f71dd6be041faacb6edc99f0870e9
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-26 16:57:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post("/api/registrarse", (req, res) => {
     const nombre = req.body.nombreRegistro;
     const correo = req.body.correoRegistro;
     const contrasena = req.body.contrasenaRegistro;
-    conexion.query("INSERT INTO usuarios SET ?", {nombre: nombre, apellido: "", direccion: "", correo: correo, contrasena: contrasena, sesion: 0, imagen: ""}, (error, results)=>{
+    conexion.query("INSERT INTO usuarios SET ?", {nombre: nombre, apellido: "", edad: 0, sexo: "", ciudad: "", direccion: "", correo: correo, contrasena: contrasena, sesion: 0, imagen: ""}, (error, results)=>{
         if(error){
             console.log(error);
         }else{
